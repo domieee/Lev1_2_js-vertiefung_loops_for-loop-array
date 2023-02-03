acfe68896452145bc220117e43cd77ed6be6b5cd
domieee <99103235+domieee@users.noreply.github.com>
2023-02-03 12:11:24 +0100

    first js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let numArray = [];
+
+// Deklareriert 'i' als Zählvariable
+// Pusht 'i' in das Array und erhöht 'i' um 1
+// Solange bis 'i' 10 ist
+for(let i = 0; i <= 10; i++) {
+    numArray.push(i);
+}
+
+console.log(numArray);
